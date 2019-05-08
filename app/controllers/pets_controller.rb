ed3641c758e258c807616774830c4c309b5334e5
KFad <kfadigan@yahoo.com>
2019-05-07 22:27:45 -0400

    Create Owner/Pet app

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,12 @@ class PetsController < ApplicationController
   end
 
   post "/pets" do
-    redirect to "pets/#{@pet.id}"
+    @pet = Pet.create(params[:pet])
+    unless params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    redirect "pets/#{@pet.id}"
   end
 
   get "/pets/:id" do
@@ -20,6 +25,17 @@ class PetsController < ApplicationController
   end
 
   post "/pets/:id" do
-    redirect to "pets/#{@pet.id}"
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    unless params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    redirect "pets/#{@pet.id}"
+  end
+
+  get "/pets/:id/edit" do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
   end
 end
