ca34cbe0e3c88498516b3764bb6ece0d75f4b866
gillianwenhold <gwenhold@mac.com>
2019-07-10 18:56:09 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "pry"
+
 class PetsController < ApplicationController
   get "/pets" do
     @pets = Pet.all
@@ -11,7 +13,16 @@ class PetsController < ApplicationController
   end
 
   post "/pets" do
+    @pet = Pet.create(params[:pet])
+    owner = Owner.create(name: params["owner"]["name"])
+    @pet.owner = owner unless params["owner"]["name"].empty?
+    @pet.save
     redirect to "pets/#{@pet.id}"
+  end
+
+  get "/pets/:id/edit" do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
   end
 
   get "/pets/:id" do
@@ -20,6 +31,11 @@ class PetsController < ApplicationController
   end
 
   post "/pets/:id" do
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    owner = Owner.create(name: params["owner"]["name"])
+    @pet.owner = owner unless params["owner"]["name"].empty?
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
